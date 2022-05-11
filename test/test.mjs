e0347deecc062f96c8a8e75ff3d3470dff86733b
Eric <airkro@qq.com>
2022-05-11 13:48:15 +0800

    refactor: logic

--- test/test.mjs
+++ test/test.mjs
@@ -1,7 +1,31 @@
 // eslint-disable-next-line import/no-unresolved
 import test from 'ava';
 
+import { processor } from '../lib/processor.cjs';
+
 import { eslint, html } from './helper/utils.mjs';
+
+test('preprocess', (t) => {
+  const code = html`
+    <script type="module">
+      'use strict';
+    </script>
+    <script>
+      alert();
+    </script>
+  `;
+
+  const [first, second] = processor.preprocess(code, 'fake.htm');
+
+  t.deepEqual(first, {
+    filename: 'fake.htm/0/html-script-module.js',
+    text: "\n                          \n      'use strict';\n    ",
+  });
+  t.deepEqual(second, {
+    filename: 'fake.htm/1/html-script-legacy.js',
+    text: '\n                          \n                   \n             \n            \n      alert();\n    ',
+  });
+});
 
 test('module', async (t) => {
   const code = html`
